8c36fa00e0abcff1c69402e61e8d3c03c19b68e4
Gabriel Tavares <gabrielgstell@gmail.com>
2023-11-14 14:24:44 -0300

    Funcionario e Produto com o CRUD completo, porém, o visual mostra erro mesmo estando funcionando

--- scr/static/script/script.js
+++ scr/static/script/script.js
@@ -12,7 +12,6 @@ function carregarImagem()
     {
         var img = document.getElementById("img");
         img.src = event.target.result;
-        
     }
 }
 // __________________________________________________________________
